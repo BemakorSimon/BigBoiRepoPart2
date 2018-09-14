ba0f62180377b8ff56f2b28654ef06e34ea8d76a
BemakorSimon <43002710+BemakorSimon@users.noreply.github.com>
2018-09-14 08:29:26 -0400

    Add files via upload

---
+++ First Project/src/FirstClass.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author Simon Gajewski
+ * First Submission
+ */
+public class FirstClass 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.print("Helllo Period 1");
+
+	}
+
+}
+
