0d345a75fcde6b8ce74f9164cb7b90041c952783
BemakorSimon <43002710+BemakorSimon@users.noreply.github.com>
2018-09-26 13:23:12 -0700

    Add files via upload

---
+++ First Project/src/VariableTest.java
@@ -0,0 +1,19 @@
+
+public class VariableTest 
+{
+
+	public static void main(String[] args)
+	{
+		String dayOfWeek;
+		int timeSpent;
+		double pieNum;
+		
+		pieNum = 3.14;
+		timeSpent = 10;
+		dayOfWeek ="Wendsday";
+		System.out.print("Today is "+ dayOfWeek + " and I spent " + timeSpent + 
+				" Minutes on this.\nAlso, Pie is equal to " + pieNum);
+
+	}
+
+}
