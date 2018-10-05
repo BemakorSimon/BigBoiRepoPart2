a174692493a8382b25ee0a643f7d6e9d8c6cb129
BemakorSimon <43002710+BemakorSimon@users.noreply.github.com>
2018-10-05 05:40:01 -0700

    Add files via upload

---
+++ PleaseWork.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class PleaseWork 
+{
+
+	
+	public static void main(String[] args)
+	{
+		Scanner jawn = new Scanner(System.in);
+		System.out.print("First name of person 1:");
+		String firstName = jawn.nextLine();
+		System.out.print("Gender of person 1:");
+		String genderofPerson2 = jawn.nextLine();
+		System.out.print("Animal 1(plural):");
+		String animal1 = jawn.nextLine();
+		System.out.print("color:");
+		String animal2 = jawn.nextLine();
+		System.out.println("First name2:");
+		String firstname2 = jawn.nextline();
+		System.out.println(Gender of Person 2:);
+		String genderofPerson2 = jawn.nextLine();
+		System.out.println(Adjective:);
+		String 
+
+	}
+
+}
