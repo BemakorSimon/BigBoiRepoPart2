d6ed8a809451fdc6e7e787b5cc0302696f04cbb2
BemakorSimon <43002710+BemakorSimon@users.noreply.github.com>
2018-10-15 21:17:17 -0400

    Update GeometryCalculator.java

--- src/GeometryCalculator.java
+++ src/GeometryCalculator.java
@@ -1,19 +1,51 @@
 import java.util.Scanner;
 
-public class GeometryCalculator 
+
+//*******
+//
+// ParseInput - processes input from user and calls Geometry Calculator
+//
+public class ParseInput 
 {
 
 	public static void main(String[] args) 
 	{
+		// Create a Scanner Object
 		Scanner jawn = new Scanner(System.in);
-		double base;
-		double height; 
+		
+		// Get name and numbers from user
 		System.out.println("Please enter your name: ");
 		String name = jawn.nextLine(); 
 		System.out.println("\n\nHello, " + name 
-				+ ", welcome to Geometry Calculator!\nPlease press enter to continue...");
-		jawn.nextLine();
-		//https://stackoverflow.com/questions/13102045/scanner-is-skipping-nextline-after-using-next-or-nextfoo
+				+ ", welcome to Geometry Calculator!\n\nEnter base value: ");
+		String base = jawn.nextLine(); 
+		System.out.println("\nEnter height value: ");
+		String height = jawn.nextLine(); 
+        
+        // Create a Calculator
+        GeometryCalculator myCalc = new GeometryCalculator();
+        double area = myCalc.calculateTriangleArea(Double.parseDouble(base),Double.parseDouble(height));
+
+        // Print result to user
+        System.out.println("\n\nArea of Triangle is, " + area); 
+
 	}
 
+}
+//**********
+//
+// GeometryCalculator - just calculates an area given 2 values
+//
+class GeometryCalculator 
+{
+    double base;
+    double height;
+    
+    GeometryCalculator() {
+        base=0;
+        height=0;
+    }
+    public double calculateTriangleArea(double base, double height) {
+        return (0.5 * base) * height;
+    }
 }
