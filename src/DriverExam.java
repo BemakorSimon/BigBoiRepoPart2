c5710a3be06a68e2d90f4778dc234f84e93af98e
BemakorSimon <43002710+BemakorSimon@users.noreply.github.com>
2019-03-08 08:38:25 -0500

    Add files via upload

--- src/DriverExam.java
+++ src/DriverExam.java
@@ -1,4 +1,61 @@
 
 public class DriverExam {
-
+	private char[] correctAnswers;
+	
+	public DriverExam (char[] answerKey) {
+		correctAnswers = new char[answerKey.length];
+		for (int i = 0; i < answerKey.length; i++)
+			correctAnswers[i] = answerKey[i];
+	}//answersetter
+	
+	
+	public int totalCorrect (char[] studentAnswers) {
+		int correctAnsNum = 0;
+		for (int i = 0; i < correctAnswers.length; i++) {
+			if (studentAnswers[i] == correctAnswers[i])
+				correctAnsNum++;
+		}
+		return correctAnsNum;
+	}//totalCorrect
+	
+	
+	public int totalIncorrect (char[] studentAnswers) {
+		int incorrectAnsNum = 0;
+		for (int i = 0; i < correctAnswers.length; i++) {
+			if (studentAnswers[i] != correctAnswers[i])
+				incorrectAnsNum++;
+		}
+		return incorrectAnsNum;
+	}//totalCorrect
+	
+	
+	public boolean passed (char[] studentAnswers) {
+		double grade = ((double)totalCorrect(studentAnswers))/(correctAnswers.length);
+		System.out.println(grade);
+		boolean didPass = false;
+		if (grade >= 0.75) {
+			didPass = true;
+		}
+		return didPass;
+	}//StudentAnswers
+	
+	public int[] questionsMissed (char[] studentAnswers) {
+		int[] wrongQuestions;
+		int counter = 0;
+		counter = totalIncorrect(studentAnswers);
+		wrongQuestions = new int[counter];
+		for (int i = 0; i < correctAnswers.length; i++) {
+			int arrayCounter = 0;
+			if (correctAnswers[i] == studentAnswers[i]) {
+				System.out.println("Question " + (i+1) + " is right");
+			}
+			else {
+				System.out.println("Question " + (i+1) + " is wrong");
+				wrongQuestions[arrayCounter] = i;
+			}	
+			arrayCounter = i;
+			
+		}
+		return wrongQuestions;
+	}
 }
--- src/mainDriverExam.java
+++ src/mainDriverExam.java
@@ -5,8 +5,12 @@ public class mainDriverExam {
 		char[] answerKey = {'a','b','c','d','e','f'};
 		DriverExam exam = new DriverExam(answerKey);
 		char[] student = {'a','b','d','d','e','g'};
-		System.out.println(DriverExam.incorrectAnswers(student));
-		
+		System.out.println(exam.totalIncorrect(student));
+		System.out.println(exam.totalCorrect(student));
+		System.out.println(exam.passed(student));
+		int[] wrongAnswers = exam.questionsMissed(student);
+		for (int i = 0; i < wrongAnswers.length; i++)
+			System.out.println(wrongAnswers[i]);
 	}
 
 }
